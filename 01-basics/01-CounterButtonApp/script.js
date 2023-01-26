ae0d42bedbb20d760945d7e9fd18e07172572d02
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-01-26 14:27:36 +0600

    01-CounterButtonApp solved

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,28 @@
 // import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+// import { template } from 'lodash'
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js'
+
+// const button = document.querySelector('button')
+
+const App = defineComponent({
+
+    name:'App',
+
+    data(){
+        return {
+            hello: 'world',
+            counter: 0,
+        }
+    },
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm;
+
+
