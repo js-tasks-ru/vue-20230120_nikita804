0c2dad36ab6543bcf2981665976444a15a7a99da
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-01-29 18:26:05 +0600

    02-VueCalculator solved (with watch)

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,56 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js'
 
-// Создайте Vue приложение
+const App = defineComponent({
+
+    name:'App',
+
+    data(){
+        return {
+            inputs: {
+                input1: 0,
+                input2: 0,
+            },
+            result: 0,
+            operation: '',
+        }
+    },
+
+    methods:{
+        calculate(){
+            if (this.operation === 'sum'){
+                return this.inputs.input1 + this.inputs.input2
+            } else if (this.operation === 'subtract'){
+                return (this.inputs.input1 - this.inputs.input2)
+            } else if (this.operation === 'multiply'){
+                return (this.inputs.input1 * this.inputs.input2)
+            } else if (this.operation === 'divide'){
+                return (this.inputs.input1 / this.inputs.input2)
+            }
+        }
+    },
+
+    watch:{
+
+        inputs: {
+            immediate: true,
+            deep: true,
+            handler(){
+                this.result = this.calculate()
+            }
+        },
+
+        operation: {
+            immediate: true,
+            deep: true,
+            handler(){
+                this.result = this.calculate()
+            }
+        },
+
+    },
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
