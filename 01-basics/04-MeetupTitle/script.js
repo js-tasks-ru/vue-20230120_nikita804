4661c20e8c58299be64245bc454f763fa1457886
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-01-31 19:49:13 +0600

    04-MeetupTitle solved

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js'
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,36 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+
+    name:'App',
+
+    data(){
+        return {
+            inputValue: 1,
+            meetupData: '',
+            meetupTitle: 'MEETUP_TITLE',
+        }
+    },
+
+    watch:{
+      inputValue: {
+        immediate: true,
+        deep: true,
+        handler(){
+          fetchMeetupById(this.inputValue)
+            .then((meetupData) => {
+              this.meetupData = meetupData;
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+      }
+     
+    }
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
