ffd32255a82118b1da194a3492d993de87378fdf
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-02-04 19:14:10 +0600

    ¯\_(ツ)_/¯

---
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -0,0 +1,27 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'CounterButton',
+
+  props:{
+    count:{
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  methods:{
+    increment(value){
+      this.$emit('update:count', value + 1);
+    } ,
+  },
+
+
+
+  template: `<button 
+  type="button"
+  @click="increment(count)"
+  >{{count}}</button>`,
+});
