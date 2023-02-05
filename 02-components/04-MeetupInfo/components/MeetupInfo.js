8db5fe7734f44b03a11c4c29e6fd0b3b0a57f21b
Nikita Bychkov <nikitabychkov96@gmail.com>
2023-02-05 22:20:34 +0600

    02-04 solved

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -3,19 +3,50 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupInfo',
 
+  props:{
+    organizer:{
+      type: String,
+      required: true,
+    },
+    place:{
+      type: String,
+      required: true,
+    },
+    date:{
+      type: Number,
+      required: false,
+    },
+  },
+
+  // emits: ['update:count'],
+
+  // methods:{
+  //   increment(value){
+  //     this.$emit('update:count', value + 1);
+  //   } ,
+  // },
+
+  computed: {
+    formattedDate() {
+      const date = new Date(this.date);
+      const options = { day: 'numeric', month: 'long', year: 'numeric' };
+      return date.toLocaleDateString('ru-RU', options);
+    },
+  },
+
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{organizer}}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{place}}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time datetime="2020-01-01">{{formattedDate}}</time>
       </li>
     </ul>`,
 });
